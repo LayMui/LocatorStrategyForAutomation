664a285d41f8ed972c1a70d9737e47eb37351326
LayMui <laymui.toh@taiger.com>
2021-03-13 15:56:54 +0800

    Link to youtube channel for last page

--- src/views/pages/locatorStrategy16.vue
+++ src/views/pages/locatorStrategy16.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="text-center">
-    <img contain width=50% height=50% src="../../assets/image/locatorStrategy16.png"/>
-
-   
+     <p class="text-muted left-aligned">
+      Watch this Youtube #1: <a href="
+  https://www.youtube.com/watch?v=2hW3jj__32Y&feature=youtu.be
+" target="_blank">
+  usingAccessiblityLabel for React Native iOS App</a>
+    </p>
+     <p class="text-muted left-aligned">
+      Watch this Youtube #2: <a href="
+  https://www.youtube.com/watch?v=QonGhzZDOmE&feature=youtu.be
+" target="_blank">
+  using accessbilityLabel On React native Android App</a>
+    </p>
   </div>
 </template>
